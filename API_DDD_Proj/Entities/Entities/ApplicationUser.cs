fa3604b1e8e131e0c4eadecb24118bad7edf944e
Erick.Sabino <felerick@gmail.com>
2022-07-01 03:09:33 -0300

    ApplicationUser has been modified

--- API_DDD_Proj/Entities/Entities/ApplicationUser.cs
+++ API_DDD_Proj/Entities/Entities/ApplicationUser.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Entities.Enums;
+using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,5 +11,16 @@ namespace Entities.Entities
 {
     public class ApplicationUser : IdentityUser
     {
+        [Column("USR_AGE")]
+        public int Age { get; set; }
+
+        [Column("USR_CELLPHONENUMBER")]
+        public string CellphoneNumber { get; set; }
+
+        [Column("USR_TYPE")]
+        public UserType? Tipe { get; set; }
+
+
+
     }
 }
